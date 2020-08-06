5ac3243dfe28189c32b85b0a0efa24ce0c88f32d
SanketTambe39 <sankettambe39@gmail.com>
2020-08-06 17:17:17 +0530

    Percentage Of Triplet Output

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -25,12 +25,16 @@ flipCoin $heads $tails ${SingleArray[@]}
 
 singletDict[heads]=$heads
 singletDict[tails]=$tails
-
+echo ""
+echo ":::::::::::::::::::::::::: Singlet Coin Toss :::::::::::::::::::::::::::"
+echo ""
 for i in ${!singletDict[@]}
 do
 	echo " In singlet We get" $i "for" ${singletDict[$i]}"%"
 done
-
+echo ""
+echo ":::::::::::::::::::::::::: Doublet Coin Toss :::::::::::::::::::::::::::"
+echo ""
 for ((i=0,j=0;i<100;i+=2,j+=1))
 do
 	sample1=${SingleArray[$i]}
@@ -61,4 +65,56 @@ echo " In Doublet Combination we get HT :: "$count_HT"%"
 echo " In Doublet Combination we get HH :: "$count_HH"%"
 echo " In Doublet Combination we get TT :: "$count_TT"%"
 echo " In Doublet Combination we get TH :: "$count_TH"%"
+echo ""
+echo ":::::::::::::::::::::::::: Triplet Coin Toss :::::::::::::::::::::::::::"
+echo ""
+for ((i=0,j=0;i<100;i+=3,j+=1))
+do
+        sample1=${SingleArray[$i]}
+        sample2=${SingleArray[$i+1]}
+	sample3=${SingleArray[$i+2]}
 
+        CheckTriplet[$j]="$sample1$sample2$sample3"
+
+        case ${CheckTriplet[$j]} in
+                "HHH")
+                        count_HHH=$(($count_HHH+1))
+                ;;
+                "HHT")
+                        count_HHT=$(($count_HHT+1))
+                ;;
+                "HTH")
+                        count_HTH=$(($count_HTH+1))
+                ;;
+                "THH")
+                        count_THH=$(($count_THH+1))
+                ;;
+		"HTT")
+			count_HTT=$(($count_HTT+1))
+		;;
+		"THT")
+			count_THT=$(($count_THT+1))
+		;;
+		"TTH")
+                        count_TTH=$(($count_TTH+1))
+                ;;
+		"TTT")
+			count_TTT=$(($count_TTT+1))
+		;;
+		*)
+			extra=$(($extra+1))
+		;;
+        esac
+done
+
+#echo ${CheckTriplet[@]}
+
+echo " In Doublet Combination we get HHH :: "$count_HHH"%"
+echo " In Doublet Combination we get HHT :: "$count_HHT"%"
+echo " In Doublet Combination we get HTH :: "$count_HTH"%"
+echo " In Doublet Combination we get THH :: "$count_THH"%"
+echo " In Doublet Combination we get HTT :: "$count_HTT"%"
+echo " In Doublet Combination we get THT :: "$count_THT"%"
+echo " In Doublet Combination we get TTH :: "$count_TTH"%"
+echo " In Doublet Combination we get TTT :: "$count_TTT"%"
+echo " There are Some Extra Toss Remaining they are :: "$extra"%"
